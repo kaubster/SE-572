7d3b783c258bdc8e78a6430c6664d997d81cddc0
kaubin <kaubin@sonalysts.com>
2020-06-22 21:44:41 -0400

    bit of styling, text and table css wip

--- web/public/films.js
+++ web/public/films.js
@@ -1,84 +1,84 @@
 var API = (() => {
-  const NOT_FOUND = null;
+    const NOT_FOUND = null;
 
-  var films = [
-    //// Some test data...
-    // {
-    //   name: "movie1",
-    //   rating: 100,
-    // },
-    // {
-    //   name: "movie3",
-    //   rating: 100,
-    // },
-    // {
-    //   name: "movie2",
-    //   rating: 100,
-    // },
-  ];
+    var films = [
+        //// Some test data...
+        // {
+        //   name: "movie1",
+        //   rating: 100,
+        // },
+        // {
+        //   name: "movie3",
+        //   rating: 100,
+        // },
+        // {
+        //   name: "movie2",
+        //   rating: 100,
+        // },
+    ];
 
-  var findFilmByName = (myArray, key) => {
-    for (let i = 0; i < myArray.length; i++) {
-      if (myArray[i].name === key) {
-        return myArray[i];
-      }
-    }
-  };
+    var findFilmByName = (myArray, key) => {
+        for (let i = 0; i < myArray.length; i++) {
+            if (myArray[i].name === key) {
+                return myArray[i];
+            }
+        }
+    };
 
-  var filmsExist = () => {
-    if (films.length == 0) return false;
-    return true;
-  };
+    var filmsExist = () => {
+        if (films.length == 0) return false;
+        return true;
+    };
 
-  var clearTable = (table) => {
-    if (table) {
-      while (table.hasChildNodes()) {
-        table.removeChild(table.firstChild);
-      }
-    }
-  };
+    var clearTable = (table) => {
+        if (table) {
+            while (table.hasChildNodes()) {
+                table.removeChild(table.firstChild);
+            }
+        }
+    };
 
-  var generateTable = (table, arrayOfFilms) => {
-    for (var r = 0; r < arrayOfFilms.length; r++) {
-      var x = table.insertRow(r);
+    var generateTable = (table, arrayOfFilms) => {
+        for (var r = 0; r < arrayOfFilms.length; r++) {
+            var x = table.insertRow(r);
 
-      var left = x.insertCell(0);
-      left.innerHTML = arrayOfFilms[r].name;
+            var left = x.insertCell(0);
+            left.innerHTML = arrayOfFilms[r].name;
 
-      var right = x.insertCell(1);
-      right.innerHTML = arrayOfFilms[r].rating;
-    }
-  };
+            var right = x.insertCell(1);
+            right.innerHTML = arrayOfFilms[r].rating;
+        }
+    };
 
-  var createFilm = () => {
-    let filmNameBx = document.getElementById("newFilmName");
+    var createFilm = () => {
+        let filmNameBx = document.getElementById("newFilmName");
+        let name = filmNameBx.value;
+        if (name) {
+            if (NOT_FOUND == findFilmByName(films, name)) {
+                let record = {
+                    name: name,
+                    rating: 100,
+                };
 
-    let name = filmNameBx.value;
+                films.push(record);
+            }
+        }
+        return false;
+    };
 
-    if (NOT_FOUND == findFilmByName(films, name)) {
-      let record = {
-        name: name,
-        rating: 100,
-      };
+    var getFilms = () => {
+        if (filmsExist()) {
+            let table = document.getElementById("filmTable");
 
-      films.push(record);
-    }
-    return false;
-  };
+            clearTable(table);
 
-  var getFilms = () => {
-    if (filmsExist()) {
-      let table = document.getElementById("filmTable");
+            generateTable(table, films);
+        }
+        return false;
+    };
 
-      clearTable(table);
-
-      generateTable(table, films);
-    }
-    return false;
-  };
-
-  return {
-    createFilm,
-    getFilms,
-  };
+    return {
+        createFilm,
+        getFilms,
+    };
 })();
