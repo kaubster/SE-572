a67bb2b25d2c5bad1a4af4b634d768daf37ba0ac
Keith Aubin <kaubin@sonalysts.com>
2020-08-13 15:24:44 -0400

    mobile-web

---
+++ mobile-web/screens/AuthContext.tsx
@@ -0,0 +1,5 @@
+import React, { Component, useState } from "react";
+
+// Make sure the shape of the default value passed to
+// createContext matches the shape that the consumers expect!
+export const AwtContext = React.createContext("awtToken");
